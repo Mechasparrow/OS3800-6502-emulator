9ac6a2cdb31642744838b0e2aae3fc86e348b603
Michael Navazhylau <mankbf@umsystem.edu>
2022-03-30 18:03:47 -0500

    Added VASM assembler for test programs

---
+++ test_assembly/test.asm
@@ -0,0 +1,11 @@
+; Input:  A NUL-terminated, <255-length pattern at address PATTERN.
+;         A NUL-terminated, <255-length string pointed to by STR.
+;
+; Output: Carry bit = 1 if the string matches the pattern, = 0 if not.
+;
+; Notes:  Clobbers A, X, Y. Each * in the pattern uses 4 bytes of stack.
+;
+
+program:
+	LDA #$FF
+	STA $01
