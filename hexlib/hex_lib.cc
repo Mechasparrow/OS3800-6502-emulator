b4802c3330ad98924ee4434307725120047fb72c
Michael Navazhylau <mankbf@umsystem.edu>
2022-03-30 09:54:01 -0500

    Fixed broken hex tesT

--- hexlib/hex_lib.cc
+++ hexlib/hex_lib.cc
@@ -1,41 +1,15 @@
 #include "hex_lib.h"
 
-using std::map;
-
-map<uint8_t, char> getByteToHexMap(){
-    unsigned char HEX[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    map<uint8_t, char> hexMap;
-
-    for (uint8_t i = 0; i < 16; i++){
-        hexMap.insert(hexMap.begin(), std::pair<uint8_t, char> (i,HEX[i]) );
-    }
-
-    return hexMap;
-}
-
-map<char, uint8_t> getHexToByteMap(){
-    unsigned char HEX[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    map<char, uint8_t> hexMap;
-
-    for (uint8_t i = 0; i < 16; i++){
-        hexMap.insert(hexMap.begin(), std::pair<char, uint8_t> (HEX[i], i) );
-    }
-
-    return hexMap;
-}
-
 uint8_t hex2byte(std::string hexstring) {
     unsigned char higherByte = toupper(hexstring[0]);
     unsigned char lowerByte = toupper(hexstring[1]);
-
-    map<char,uint8_t> hexMap = getHexToByteMap();
 
     uint8_t mappedLowerHexValue;
     uint8_t mappedHigherHexValue;
 
     try {
-        mappedLowerHexValue = hexMap.at(lowerByte);
-        mappedHigherHexValue = hexMap.at(higherByte);
+        mappedLowerHexValue = hexToByteMap.at(lowerByte);
+        mappedHigherHexValue = hexToByteMap.at(higherByte);
     }catch (const std::exception& e){
         mappedLowerHexValue = 0;
         mappedHigherHexValue = 0;
@@ -50,13 +24,11 @@ std::string byte2hex(uint8_t byte) {
 
     std::string returnedHex = "00";
 
-    map<uint8_t, char> byteToChar = getByteToHexMap();
-
     char lowerBittleHex, upperBittleHex;
 
     try {
-        lowerBittleHex = byteToChar.at(lowerBittle);
-        upperBittleHex = byteToChar.at(upperBittle);
+        lowerBittleHex = byteToHexMap.at(lowerBittle);
+        upperBittleHex = byteToHexMap.at(upperBittle);
     }catch (const std::exception& e){
         lowerBittleHex = '0';
         upperBittleHex = '0';
--- hexlib/hex_lib.h
+++ hexlib/hex_lib.h
@@ -1,6 +1,44 @@
 #include <iostream>
 #include <map>
 
+const std::map<uint8_t, char> byteToHexMap {
+    {0,'0'},
+    {1,'1'},
+    {2,'2'},
+    {3,'3'},
+    {4,'4'},
+    {5,'5'},
+    {6,'6'},
+    {7,'7'},
+    {8,'8'},
+    {9,'9'},
+    {10, 'A'},
+    {11,'B'},
+    {12,'C'},
+    {13,'D'},
+    {14, 'E'},
+    {15, 'F'}
+};
+
+const std::map<char, uint8_t> hexToByteMap {
+    {'0',0},
+    {'1',1},
+    {'2',2},
+    {'3',3},
+    {'4',4},
+    {'5',5},
+    {'6',6},
+    {'7',7},
+    {'8',8},
+    {'9',9},
+    {'A',10},
+    {'B',11},
+    {'C',12},
+    {'D',13},
+    {'E',14},
+    {'F',15}
+};
+
 uint8_t hex2byte(std::string hexstring);
 uint16_t hex2doublebyte(std::string hexstring);
 
