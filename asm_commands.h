5e75175a288c932930e1576764234a07355fb20f
Michael Navazhylau <mankbf@umsystem.edu>
2022-03-30 12:44:12 -0500

    added asm notes

--- asm_commands.h
+++ asm_commands.h
@@ -1,3 +1,10 @@
 #include "6502_lib.h"
 
 void LDA(CPU6502 * cpu, DataBus * dataBus, uint16_t address);
+
+//TODO
+void LDA(CPU6502 * cpu, DataBus * dataBus, uint16_t address);
+
+//
+// STA -> Store A into Memory
+// Implement variant of LDA that works with constant value instead of address
